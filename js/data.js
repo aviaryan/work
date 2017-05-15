a3c1cf8efa138653b30d0b5636f86fd6a658cb8e
Avi Aryan <avi.aryan123@gmail.com>
2017-05-15 22:58:59 +0530

    add sample project list and filter feature

---
+++ js/data.js
@@ -0,0 +1,14 @@
+projects = [
+	{
+		name: 'Clipjump',
+		website: 'https://bit.ly/clipjump'
+	},
+	{
+		name: 'Shach',
+		website: 'https://github.com/aviaryan/shach'
+	},
+	{
+		name: 'Sublime 4 AutoHotkey',
+		website: 'http://git.io'
+	}
+]
--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
+// Vue.js app
+
 var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  }
+	el: '#app',
+	data: {
+		projects: projects,
+		searchInput: ''
+	},
+	watch: {
+		searchInput: function(val, oldVal){
+			searchProjects(val)
+		}
+	}
 })
+
+/*
+ * searchProject
+ * :query: The string to search for
+ */
+function searchProjects(query){
+	p = []
+	for (let i of projects){
+		if (i['name'].search(query) > -1){
+			p.push(i)
+		}
+	}
+	app.projects = p
+}
