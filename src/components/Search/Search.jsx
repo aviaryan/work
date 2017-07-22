fa5f5a333100ef6c415358fbfba003c5a6d68d2c
Avi Aryan <avi.aryan123@gmail.com>
2017-07-22 17:45:51 +0530

    search on tag click

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -4,7 +4,7 @@ import styles from './Search.css'
 export default class Search extends Component {
 	render() {
 		return (
-			<input type="text" placeholder="Filter" className={styles.search} onChange={this.props.changeHandler}/>
+			<input type="text" id="search" placeholder="Filter" className={styles.search} onChange={this.props.changeHandler}/>
 		)
 	}
 }
--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -3,9 +3,18 @@ import React, {Component} from 'react'
 import styles from './Tag.css'
 
 export default class Tag extends Component {
+	tagClick(event) {
+		let data = event.target.textContent;
+		let search = document.getElementById('search');
+		search.value = (search.value === "") ? data : search.value + " " + data;
+		// fire event https://stackoverflow.com/questions/23892547/
+		let event2 = new Event('input', { bubbles: true });
+		search.dispatchEvent(event2);
+	}
+
 	render() {
 		return (
-			<span className={styles.tag}>
+			<span className={styles.tag} onClick={this.tagClick}>
 				{this.props.name}
 			</span>
 		)
