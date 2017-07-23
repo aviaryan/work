a159d9ec44cdc223021f17c96c790b7e6d3f438e
Avi Aryan <avi.aryan123@gmail.com>
2017-07-23 07:45:59 +0530

    improved search behavior

--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -6,7 +6,14 @@ export default class Tag extends Component {
 	tagClick(event) {
 		let data = event.target.textContent;
 		let search = document.getElementById('search');
-		search.value = (search.value === "") ? data : search.value + " " + data;
+		let vals = search.value.split(' ');
+		let dataIndex = vals.indexOf(data);
+		if (dataIndex > -1){ // exists, remove it
+			vals.splice(dataIndex, 1);
+		} else {
+			vals.push(data);
+		}
+		search.value = vals.join(' ');
 		// fire event https://stackoverflow.com/questions/23892547/
 		let event2 = new Event('input', { bubbles: true });
 		search.dispatchEvent(event2);
--- src/screens/Page404/Page404.jsx
+++ src/screens/Page404/Page404.jsx
@@ -9,6 +9,7 @@ export default class Page404 extends Component {
 		return (
 			<div className={appStyles.app}>
 				<div className={styles.text404}>404</div>
+				<div className={styles.detail404}>Head back <a href="#">home</a></div>
 			</div>
 		);
 	}
