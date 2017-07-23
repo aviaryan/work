30f6d7b41187b1d47276581ffb3e1db076823ec0
Avi Aryan <avi.aryan123@gmail.com>
2017-07-23 08:40:34 +0530

    add more data to project detail

--- src/screens/ProjectDetail/ProjectDetail.jsx
+++ src/screens/ProjectDetail/ProjectDetail.jsx
@@ -4,7 +4,7 @@ import lib from '../../lib/utils'
 import appStyles from '../App/App.css'
 import styles from './ProjectDetail.css'
 import Page404 from '../Page404/Page404'
-import ProjectLinks from '../../components/ProjectLinks/ProjectLinks'
+import Tag from '../../components/Tag/Tag'
 
 
 export default class ProjectDetail extends Component {
@@ -16,19 +16,41 @@ export default class ProjectDetail extends Component {
 				<Page404/>
 			);
 		}
-		// normal
+		// tags and websites
+		let tagsDOM = project.tags.map((t) =>
+			<Tag key={t} name={t}/>
+		);
+		let websitesDOM = [];
+		if (project.websites) {
+			websitesDOM = project.websites.map((w) =>
+				<div key={w}>Website: <a href={w}>{w}</a></div>
+			);
+		}
+		// render
 		return (
 			<div className={[appStyles.app, styles.page].join(' ')}>
 				<div className={styles.name}>
+					<a href="#" className={styles.backLink}>←</a>
 					{project.name}
 				</div>
+
+				<div className={styles.hr} />
+
 				<div className={styles.links}>
-					<ProjectLinks {...project}/>
+					{project.github &&
+					<div>GitHub: <a href={project.github}>{project.github}</a></div>}
+					{websitesDOM}
 				</div>
+
 				<div className={styles.desc}>
 					{project.desc}
 					<p>{project.more}</p>
 				</div>
+
+				<div className={styles.tags}>
+					{tagsDOM}
+				</div>
+
 			</div>
 		);
 	}
