6012ce0c371954dfec91e7d8a23cc7e1ed7bff90
Avi Aryan <avi.aryan123@gmail.com>
2017-07-25 14:53:20 +0530

    mobile improvements in detail view

--- src/components/GitHubStar/GitHubStar.jsx
+++ src/components/GitHubStar/GitHubStar.jsx
@@ -4,10 +4,16 @@ export default class GitHubStar extends Component {
 	render() {
 		let re = new RegExp('.*github[^\/]*\/([^\/]+)\/([^/\#]+)($|.*)', 'i');
 		let found = this.props.github.match(re);
+		let width = window.innerWidth;
+		let size = 'large', height='30px';
+		if (width < 550) {
+			size = 'none';
+			height = '20px';
+		}
 		return (
 			<iframe
-				src={'https://ghbtns.com/github-btn.html?user=' + found[1] + '&repo=' + found[2] + '&type=star&count=true&size=large'}
-				frameBorder="0" scrolling="0" width="160px" height="30px" />
+				src={'https://ghbtns.com/github-btn.html?user=' + found[1] + '&repo=' + found[2] + '&type=star&count=true&size=' + size}
+				frameBorder="0" scrolling="0" width="160px" height={height} />
 		);
 	}
 }
