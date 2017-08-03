375ea1de9b0a45a565e13bd7b7a68d4af47e4a40
Avi Aryan <avi.aryan123@gmail.com>
2017-08-03 20:05:55 +0530

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# yarn build
+
+# copy build files
+rm -rf deploy/
+mkdir -p deploy
+cp -f index.html deploy/
+mkdir -p deploy/dist
+cp -fR dist/ deploy/dist/
+mkdir -p deploy/img
+cp -fR img/ deploy/img/
+
+git checkout gh-pages
+cp -fR deploy/ .
+
+rm -rf deploy/
+git add .
+git commit -m "deploy"
+git push origin gh-pages
+
+git checkout pivot-to-react
---
+++ src/data/colors.js
@@ -0,0 +1,33 @@
+/*
+ * Thanks to geopattern for the colors
+ */
+export default [
+	{
+		name: 'dullindigo',
+		color: '#A6C6EC'
+	},
+	{
+		name: 'dullgreen',
+		color: '#B0EDBB'
+	},
+	{
+		name: 'dullyellow',
+		color: '#F1E4A4'
+	},
+	{
+		name: 'dullorange',
+		color: '#F7D19A'
+	},
+	{
+		name: 'dullred',
+		color: '#EA9AA6'
+	},
+	{
+		name: 'dullviolet',
+		color: '#DCA4EB'
+	},
+	// {
+	// 	name: 'dullblue',
+	// 	color: '#A3CBEF'
+	// }
+];
